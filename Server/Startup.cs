ab3e0a322d5f71d31c902c373f62c83933987191
jsakamoto <junichi.jp@gmail.com>
2020-04-22 14:34:52 +0900

    Make it to be configurable base URL via command line.

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -22,6 +22,8 @@ namespace BlazorWOL.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(this.Configuration);
+            services.AddRazorPages();
             services.AddControllersWithViews();
 
             services.AddSingleton(serviceProvider =>
@@ -57,7 +59,7 @@ namespace BlazorWOL.Server
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
-                endpoints.MapFallbackToFile("index.html");
+                endpoints.MapFallbackToPage("/_Host");
             });
         }
     }
