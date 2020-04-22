ab82e7c75d61febfc8b424972bb4ee7f5f40a899
jsakamoto <junichi.jp@gmail.com>
2020-04-22 10:05:49 +0900

    Make it installable/uninstallable as a service from command line.

---
+++ Server/Internals/WindowsServiceTool.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
+
+namespace BlazorWOL.Server.Internals
+{
+    internal static class WindowsServiceTool
+    {
+        public enum StartUp
+        {
+            Auto,
+            Demand,
+            Disabled
+        }
+
+        private static string ExecutableFilePath => Process.GetCurrentProcess().MainModule.FileName;
+
+        private static bool IsAdministrator => new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+
+        private static void RunAsAdmin(string fileName, params string[] args) => Run(true, fileName, args);
+
+        private static void Run(string fileName, params string[] args) => Run(false, fileName, args);
+
+        private static void Run(bool runAsAdmin, string fileName, params string[] args)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                UseShellExecute = false,
+                FileName = fileName,
+                Arguments = string.Join(" ", args.Select(arg => arg.Contains(" ") ? "\"" + arg.Replace("\"", "\\\"") + "\"" : arg)),
+            };
+            if (runAsAdmin)
+            {
+                startInfo.UseShellExecute = true;
+                startInfo.Verb = "runas";
+            }
+
+            Process.Start(startInfo).WaitForExit();
+        }
+
+        public static void RegisterToWindowsService(string serviceName, string displaName, string description, StartUp startUp = StartUp.Auto)
+        {
+            if (!IsAdministrator)
+            {
+                RunAsAdmin(ExecutableFilePath, "install");
+                return;
+            }
+
+            var startupText = startUp switch
+            {
+                StartUp.Auto => "auto",
+                StartUp.Demand => "demand",
+                StartUp.Disabled => "disabled",
+                _ => throw new Exception()
+            };
+
+            Run("sc", "create",
+                serviceName,
+                "binpath=", $"\"{ExecutableFilePath}\" --service",
+                "start=", startupText,
+                "displayname=", displaName
+            );
+
+            Run("sc", "description", serviceName, description);
+        }
+
+        public static void UnregisterToWindowsService(string serviceName)
+        {
+            if (!IsAdministrator)
+            {
+                RunAsAdmin(ExecutableFilePath, "uninstall");
+                return;
+            }
+
+            Run("net", "stop", serviceName);
+            Run("sc", "delete", serviceName);
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -9,19 +9,36 @@ namespace BlazorWOL.Server
 {
     public class Program
     {
+        private const string ServiceName = "BlazorWOL";
+
+        private const string DisplayName = "Blazor Wakeup On Lan";
+
+        private const string Description = "Provide a Web UI to sending Wakeup On Lan magic packet.";
+
         public static async Task Main(string[] args)
         {
             Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
-            var isWinSvcMode = args.Contains("--service");
-            var hostBuilder = CreateHostBuilder(args);
-            if (isWinSvcMode)
+            if (Enumerable.SequenceEqual(args, new[] { "install" }))
             {
-                await hostBuilder.RunAsServiceAsync();
+                WindowsServiceTool.RegisterToWindowsService(ServiceName, DisplayName, Description);
+            }
+            else if (Enumerable.SequenceEqual(args, new[] { "uninstall" }))
+            {
+                WindowsServiceTool.UnregisterToWindowsService(ServiceName);
             }
             else
             {
-                await hostBuilder.RunConsoleAsync();
+                var isWinSvcMode = args.Contains("--service");
+                var hostBuilder = CreateHostBuilder(args);
+                if (isWinSvcMode)
+                {
+                    await hostBuilder.RunAsServiceAsync();
+                }
+                else
+                {
+                    await hostBuilder.RunConsoleAsync();
+                }
             }
         }
 
